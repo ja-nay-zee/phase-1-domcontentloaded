d34fe20a0764edc3080e3877ea39d022bc381563
Yaneizy Castillo <yccastillo18@gmail.com>
2021-08-17 13:45:34 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+//first try
+const event = document.addEventListener("DOMContentLoaded", function() {
+     document.getElementById("text")
+    .innerHTML = "This is really cool!"
+});
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     newDOM()
+// });
+
+// function newDOM() {
+//     document.getElementById("text")
+//     .innerHTML = "This is really cool!"
+// }
+
+
+///// solution on Git Hub
+// document.addEventListener( "DOMContentLoaded", function () {
+//     updateDOM()
+//   } );
+  
+  
+//   function updateDOM() {
+//     document.getElementById( "text" )
+//       .innerHTML = "This is really cool!";
+//   }
